4affc7213de25b957ffd74f718e3d5401dcba88d
stellarpower <stellarpower@googlemail.com>
2024-04-20 18:02:24 +0000

    Rename some variables and lint

--- softdtwkeras/SDTWLoss.py
+++ softdtwkeras/SDTWLoss.py
@@ -32,49 +32,80 @@ class SDTWLoss(tf.keras.losses.Loss):
         """
         # return pairwise euclidean difference matrix
         Args:
-          A,    [m,d] matrix
-          B,    [n,d] matrix
+          A,                    [m,d] matrix
+          B,                    [n,d] matrix
         Returns:
-          D,    [m,n] matrix of pairwise distances
+          pairwiseDistances,    [m,n] matrix of pairwise distances
         """
-        D = tf.reduce_sum((tf.expand_dims(a, 1) - tf.expand_dims(b, 0)) ** 2, 2)
-        return D
+        pairwiseDistances = tf.reduce_sum(
+            (  tf.expand_dims(a, 1) - tf.expand_dims(b, 0)  ) ** 2,
+            2
+        )
+        return pairwiseDistances
+    
+
     def batch_squared_euclidean_compute_tf(self, a: tf.Tensor, b: tf.Tensor) -> tf.Tensor:
         """
         Computes pairwise distances between each elements of A and each elements of B.
         Args:
-          a,    [batch, m, d] tensor
-          b,    [batch, n, d] tensor
+          A,                    [m,d] matrix
+          B,                    [n,d] matrix
         Returns:
-          D,    [batch, m, n] tensor of pairwise distances
+          pairwiseDistances,    [m,n] matrix of pairwise distances
         """
 
         # Expand dimensions to enable broadcasting
-        a_expanded = tf.expand_dims(a, axis=2)  # Shape: [batch, m, 1, d]
-        b_expanded = tf.expand_dims(b, axis=1)  # Shape: [batch, 1, n, d]
+        a_expanded = tf.expand_dims(a, axis = 2)  # Shape: [batch, m, 1, d]
+        b_expanded = tf.expand_dims(b, axis = 1)  # Shape: [batch, 1, n, d]
 
         # Compute pairwise squared Euclidean distances
-        squared_diff = tf.reduce_sum(tf.square(a_expanded - b_expanded), axis=-1)  # Shape: [batch, m, n]
+        squared_diff = tf.reduce_sum(
+            tf.square(a_expanded - b_expanded),
+            axis = -1
+        )  # Shape: [batch, m, n]
 
         return squared_diff
+    
+    
 
     def unit_loss_from_D(self, D_):
         m, n = tf.shape(D_)[0], tf.shape(D_)[1]
 
         # Allocate memory.
-        R_ = tf.fill((m + 2, n + 2), tf.constant(np.inf, dtype=tf.float32))
-        R_ = tf.tensor_scatter_nd_update(R_, [[0, 0]], [0.0])
+        loss = tf.fill(
+            (m + 2, n + 2),
+            tf.constant(np.inf, dtype = tf.float32)
+        )
+
+        loss = tf.tensor_scatter_nd_update(
+            loss,
+            [[0, 0]],
+            [0.0]
+        )
+
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 # D is indexed starting from 0.
-                R_ = tf.tensor_scatter_nd_update(
-                    R_,
-                    [[i, j]],
-                    [D_[i - 1, j - 1] + py_softmin3(R_[i - 1, j], R_[i - 1, j - 1], R_[i, j - 1], self.gamma)]
+
+                softMinimum = py_softmin3(
+                    loss[i - 1, j    ],
+                    loss[i - 1, j - 1],
+                    loss[i    , j - 1],
+                    
+                    self.gamma
                 )
 
-        return R_[m, n]
+                loss = tf.tensor_scatter_nd_update(
+                    loss,
+                    [ [i, j] ],
+                    [ D_[i - 1, j - 1] + softMinimum ]
+                )
+
+        return loss[m, n]
+
 
     def unit_loss(self, y_true, y_pred):
+
         D_ = self.squared_euclidean_compute_tf(y_true, y_pred)
+
         return self.unit_loss_from_D(D_)
